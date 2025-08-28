33d820f2d803301e2f96290b10cec746976d182d
Vaibhavsun-30n <vaibhav@dbchat.in>
2025-08-28 13:09:53 +0530

    Added Files

---
+++ movie_search.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from typing import List
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
+logger=logging.getLogger()
+
+
+# Load dataset and create embeddings (global for testing)
+def load_data(path:str)->pd.DataFrame:
+    data=pd.read_csv(path)
+    logger.debug("Data Loaded")
+    return data
+
+# Load the Sentence Transformer model
+def load_model(name:str="all-MiniLM-L6-v2")->SentenceTransformer:
+    model = SentenceTransformer(name)
+    logger.debug("Model Loaded")
+    return model
+# Convert the 'plot of the movies into an embedding
+def plot_to_embeddings(plot:str,model:SentenceTransformer)->List:
+    return model.encode([plot], convert_to_tensor=False)
+
+def search_movies(query:str,top_n:int=3)->pd.DataFrame:
+    data=load_data("movies.csv")
+    model=load_model()
+    search_query_embedding=plot_to_embeddings(query,model)
+    data['similarity']=data['plot'].apply(lambda x:cosine_similarity(plot_to_embeddings(x,model),search_query_embedding)[0][0])
+    logger.debug("Similarity Computed")
+    return data.sort_values(by=['similarity'],ascending=False).reset_index().drop(columns=['index']).head(top_n)
+
+    
+
+    
